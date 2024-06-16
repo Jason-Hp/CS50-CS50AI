01f407dd4771da91f56861f78ac7d9a966244589
Jason-Hp <107877395+Jason-Hp@users.noreply.github.com>
2024-06-17 02:49:15 +0800

    Delete 13.sql

--- 13.sql
+++
@@ -1,3 +0,0 @@
-SELECT name from stars
-JOIN people ON stars.person_id = people.id
-WHERE stars.movie_id IN (SELECT stars.movie_id from stars WHERE stars.person_id IN (SELECT people.id from people WHERE people.name = "Kevin Bacon" AND people.birth = 1958))
